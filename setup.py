397c106fee31b9b2ae6fbbaabecc253afde5b97e
Eric Chea <eric@lightning.ai>
2022-06-17 13:35:57 -0400

    Fix setup.py naming

--- setup.py
+++ setup.py
@@ -4,12 +4,11 @@ from setuptools import find_packages, setup
 
 setup(
     name="lai_s3",
-    version="0.0.0",
-    description="⚡ Lightning component ⚡ generated with command: lightning init component",
-    author="",
-    author_email="",
-    # REPLACE WITH YOUR OWN GITHUB PROJECT LINK
-    url="https://github.com/PyTorchLightning/lightning-component-template",
+    version="0.0.1",
+    description="⚡ Lightning s3 component ⚡",
+    author="Eric Chea",
+    author_email="eric@lightning.ai",
+    url="https://github.com/Lightning-AI/LAI-s3",
     install_requires=[],
     packages=find_packages(),
 )
